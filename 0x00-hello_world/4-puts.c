028ec90b2625afac472e36bc1705e96465bad51b
Chrisadjetey25 <Chrisadjetey25@gmail.com>
2022-10-15 10:16:02 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- starting point for program execution
+ *
+ * 	Return: By default returns zero if no erros otherwise returns non-zero value.
+ *
+ *     */
+
+int main(void)
+
+{
+
+	 puts("\"Programming is like building a multilingual puzzle");
+
+	  return (0);
+
+}
