956630b0558282eee84604a7e1db6cef18d6e0ab
Chrisadjetey25 <Chrisadjetey25@gmail.com>
2022-10-17 17:14:02 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *  main-the program where the execution begins.
+ *
+ *  Return:0 if no error, non zero if error.
+ *
+ **/
+
+int main(void)
+
+{
+
+	char i;
+
+
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+
+		putchar(i);
+
+	}
+
+			putchar('\n');
+
+	return (0);
+
+}
