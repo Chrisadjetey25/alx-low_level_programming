143f1a133b361212a266b03abe3f44773c4a5148
Chrisadjetey25 <Chrisadjetey25@gmail.com>
2022-10-17 17:57:01 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main-begin program entry point.
+ *
+ * Return:0 no error, non zero if error
+ *
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+
+
+	for (i = '0'; i <= '9'; i++)
+
+		putchar(i);
+
+		putchar('\n');
+
+	return (0);
+
+}
