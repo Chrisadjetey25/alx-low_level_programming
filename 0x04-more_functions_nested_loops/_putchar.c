c6923386b1c615b998f55edb5df19fcaf57c5b55
Chrisadjetey25 <Chrisadjetey25@gmail.com>
2022-10-22 07:02:12 -0700

    Task 0

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -2,11 +2,10 @@
 #include "main.h"
 
 /**
- * _putchar - writes thhe charater c to student
- * @c: Character to print
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  * Return: on success 1 else -1
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
